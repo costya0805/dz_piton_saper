bf11107cc88d503cd23e8caa09025b80eab069d7
costya0805 <costya.co4ergin@gmail.com>
2019-10-14 15:05:16 +0500

    some changes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 map_ = []
-print("Введите параметры (ширина, высота, кол-во бомб)")
-params = input().split()
+params = input("Введите параметры (ширина, высота, кол-во бомб): ").split()
 weight = int(params[0])
 height = int(params[1])
 count_bombs = int(params[2])
@@ -9,20 +8,18 @@ for i in range(0, height):
     for j in range(0, weight):
         map_[i][j] = 0
 for n in range(1, count_bombs + 1):
-    print("Введите координаты бомбы номер "+str(n)+":")
-    location = input().split()
+    location = input("Введите координаты бомбы номер " + str(n) + ": ").split()
     x = int(location[0]) - 1
     y = int(location[1]) - 1
-    if map_[x][y] == "*":
-        print("Бомба уже здесь есть")
-    map_[x][y] = "*"
+    if x in range(0, weight) and y in range(0, height):
+        map_[x][y] = "*"
     for i in range(x - 1, x + 2):
         if i in range(0, height):
             for j in range(y - 1, y + 2):
                 if j in range(0, weight) and map_[i][j] != "*":
                     map_[i][j] += 1
 first_str = "  "
-for i in range(1, height + 1):
+for i in range(1, weight + 1):
     first_str += str(i) + " "
 print(first_str)
 for i in range(0, height):
