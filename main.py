83dcbac227364aada4c73fa86d46b00140654482
costya0805 <costya.co4ergin@gmail.com>
2019-10-14 17:23:33 +0500

    add bug catching

--- main.py
+++ main.py
@@ -11,8 +11,20 @@ for n in range(1, count_bombs + 1):
     location = input("Введите координаты бомбы номер " + str(n) + ": ").split()
     x = int(location[0]) - 1
     y = int(location[1]) - 1
-    if x in range(0, weight) and y in range(0, height):
+    if x in range(0, weight) and y in range(0, height) and map_[x][y] != "*":
         map_[x][y] = "*"
+    else:
+        all_not_ok = True
+        while all_not_ok:
+            if x < 0 or x >= weight or y < 0 or y >= height:
+                location = input("Этой точки нет на карте, пожалуста, выберите другую: ").split()
+            else:
+                location = input("На этой точке уже есть бомба, пожалуйста, введите другую: ").split()
+            x = int(location[0]) - 1
+            y = int(location[1]) - 1
+            if x in range(0, weight) and y in range(0, height) and map_[x][y] != "*":
+                map_[x][y] = "*"
+                all_not_ok = False
     for i in range(x - 1, x + 2):
         if i in range(0, height):
             for j in range(y - 1, y + 2):
